dcf049feb4573a492d1853d90384ece1540505dd
youaoi <ja01youki@gmail.com>
2017-05-06 14:15:05 +0900

    first commit.

---
+++ novelViewer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace novelViewer
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+
+        }
+    }
+}
