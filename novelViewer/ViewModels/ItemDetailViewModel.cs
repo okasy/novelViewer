3a54b968f76ad1ddaa15d578d483b9e72eca5f60
TamakiKoike <t_koike@minamachi.jp>
2017-05-22 23:40:54 +0900

    WebScraping0.0
    htmlを取得してローカルに保存。html内のタイトルタグを取得するだけ。

--- novelViewer/ViewModels/ItemDetailViewModel.cs
+++ novelViewer/ViewModels/ItemDetailViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 using Xamarin.Forms;
 
@@ -13,6 +14,18 @@ namespace novelViewer
         public Item Item { get; set; }
         public ItemDetailViewModel(Item item = null)
         {
+            string ncode = "n9669bk";
+            var scr = new WebScraping();
+            string url = "http://ncode.syosetu.com/"+ncode+"/";
+            string html = scr.GetHtml(url);
+            string title = scr.GetTitle(html);
+            item.Text = title;
+
+            string filename = ncode+".html";
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var filePath = Path.Combine(documentsPath, filename);
+            System.IO.File.WriteAllText(filePath, html);
+
             Title = item.Text;
             Item = item;
         }
